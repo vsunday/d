90dc499211bbb145c78f68232fc11f3d69a37334
vsunday <v.sunday125@gmail.com>
2024-06-17 17:50:22 +0900

    change alias command relating to docker

--- add-alias.sh
+++ add-alias.sh
@@ -5,5 +5,5 @@ echo "alias dcd='docker compose down'" >> ~/.zshrc
 e&cho "alias deb='docker exec -it "'$(docker ps -lq)'" /bin/bash'" >> ~/.zshrc
 
 # cli commands
-echo "alias box='docker run --rm -it --mount type=bind,source=$HOME/Downloads,target=/data --mount type=bind,source=$HOME/.box,target=/root/.box  bcli'"
-echo "cfp='docker run --rm -it --mount type=bind,source=$HOME/.cfp,target=/root/.cfp cfpcli'"
+echo "alias box='docker run --rm --mount type=bind,source=$HOME/Downloads,target=/data --mount type=bind,source=$HOME/.box,target=/root/.box  bcli'"
+echo "cfp='docker run --rm --mount type=bind,source=$HOME/.cfp,target=/root/.cfp cfpcli'"
